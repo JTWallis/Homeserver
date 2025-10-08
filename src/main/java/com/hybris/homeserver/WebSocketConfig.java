e277104bbbd19f129582c7a217673b687db6ea1a
Justin <wallis.justin123@gmail.com>
2025-10-08 14:18:29 +0200

    feat: added custom HandshakeHandler to /pictochat endpoint

--- src/main/java/com/hybris/homeserver/WebSocketConfig.java
+++ src/main/java/com/hybris/homeserver/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/pictochat")
-				.setAllowedOrigins("http://localhost:8001/");
+				.setAllowedOrigins("http://localhost:8001/")
+				.setHandshakeHandler(new StompHandshakeHandler());
 	}
 	
 }
