4f9c15fed8529e2b13def78d98c200c51c384945
Justin <wallis.justin123@gmail.com>
2025-10-08 14:20:53 +0200

    refactor: sendMessage endpoint changed to /topic/...

--- src/main/java/com/hybris/homeserver/pictochat/message/MessageController.java
+++ src/main/java/com/hybris/homeserver/pictochat/message/MessageController.java
@@ -11,7 +11,7 @@ public class MessageController {
 	 * Echo a message sent to /messages back to all subscribers of /room/a/messages.
 	 */
 	@MessageMapping("/messages")
-	@SendTo("/room/a/messages")
+	@SendTo("/topic/room/a/messages")
 	public Message sendMessage(Message message) throws Exception {
 		System.out.println("Received and echoing back Message" + message.toString());
 		return message;
