4aa1bec46746677420fb63f641354fe1af12c895
Justin <wallis.justin123@gmail.com>
2025-10-09 14:24:22 +0200

    docs: added comments on class and non-trivial methods

--- src/main/java/com/hybris/homeserver/pictochat/connection/RoomSubscriptionTracker.java
+++ src/main/java/com/hybris/homeserver/pictochat/connection/RoomSubscriptionTracker.java
@@ -6,6 +6,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Manages subscriptions of a user's UUID to a single, valid chat room number.
+ * A user can only subscribe to one room at a time.
+ * To disconnect/unsubscribe from a chatroom, the frontend would have to manually unsubscribe the endpoint
+ *   or terminate the WebSocket connection.
+ */
 @Component
 public class RoomSubscriptionTracker {
 
@@ -35,16 +41,31 @@ public class RoomSubscriptionTracker {
 		return userToRoom.get(user);
 	}
 	
-	public void subscribe(String user, String room) {
+	/**
+	 * Links a user to the given room number.
+	 * The room number should be checked isRoomValid beforehand.
+	 * If the user is already linked to another room, the passed room will override the subscription.
+	 * @param user UUID of the user Principal.
+	 * @param room Valid room number to link the user to.
+	 */
+	public void subscribe(String user, Character room) {
 		System.out.println("User " + user + " connected to room " + room);
 		userToRoom.put(user, room);
 	}
 	
+	/**
+	 * Unlinks a user from the room they may be linked to.
+	 * @param user UUID of the user Principal.
+	 */
 	public void unsubscribe(String user) {
 		System.out.println("User " + user + " disconnected from a room.");
 		userToRoom.remove(user);
 	}
 	
+	/**
+	 * Counts all users subscribed to each room and creates a Rooms DTO with that data.
+	 * @return New Rooms instance with the rooms array holding a new, populated Room instance for each room number.
+	 */
 	public Rooms createRoomsDto() {
 		Room[] rooms = new Room[ROOM_PREFIXES.length];
 		for(int i = 0; i < ROOM_PREFIXES.length; i++) {
