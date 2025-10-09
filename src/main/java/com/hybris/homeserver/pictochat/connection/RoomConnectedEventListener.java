34f50a29f1b2cabdc4c4b103df09f0e8b64cba6c
Justin <wallis.justin123@gmail.com>
2025-10-09 20:38:01 +0200

    docs: added comments to class and non-trivial methods

--- src/main/java/com/hybris/homeserver/pictochat/connection/RoomConnectedEventListener.java
+++ src/main/java/com/hybris/homeserver/pictochat/connection/RoomConnectedEventListener.java
@@ -13,6 +13,19 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
+/**
+ * Listens to any subscriptions/unsubscriptions to the endpoint /topic/room/{number}/messages
+ *  as well as disconnects from the WebSocket.
+ * A valid subscription will link a user UUID to one room number at a time,
+ *  broadcast the connection count of all rooms to the subscribers of /topic/connections,
+ *  and broadcast the nickname of the joining user to all subscribers of that specific room,
+ *  at the endpoint /topic/room/{number}/connections, excluding the joining user themselves.
+ * A valid unsubscription or a disconnect will unlink the user UUID,
+ *  broadcast the connection count to /topic/connections,
+ *  and broadcast the nickname of the leaving user to all subs, excluding the leaving user.
+ * The nickname should be established beforehand, by having the frontend publish it to the
+ *  endpoint /app/register.
+ */
 @Component
 public class RoomConnectedEventListener {
 	
@@ -34,6 +47,13 @@ public class RoomConnectedEventListener {
 	@Autowired
 	private SimpUserRegistry userRegistry;
 	
+	/**
+	 * Listens to all endpoint subscriptions and filters out the destination to the endpoint
+	 *  /topic/room/{roomNumber}/messages.
+	 * The user will be linked to that room number and broadcast messages will be sent to
+	 *  appropriate room subscribers.
+	 * @param event Event raised on a subscription request.
+	 */
 	@EventListener
 	public void handleSubscribeEvent(SessionSubscribeEvent event) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
@@ -73,6 +93,13 @@ public class RoomConnectedEventListener {
 		broadcastRoomsConnectionCount();
 	}
 	
+	/**
+	 * Listens to all endpoint unsubscriptions and filters out the destination to the endpoint
+	 *  /topic/room/{roomNumber}/messages.
+	 * The user will be unlinked from that room number and broadcast messages will be sent to
+	 *  appropriate room subscribers.
+	 * @param event Event raised on an unsubscription request.
+	 */
 	@EventListener
 	public void handleUnsubscribeEvent(SessionUnsubscribeEvent event) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
@@ -90,6 +117,12 @@ public class RoomConnectedEventListener {
 		}
 	}
 	
+	/**
+	 * Listens to all disconnects from the WebSocket.
+	 * The user will be unlinked from their current room number and broadcast messages will be sent to
+	 *  appropriate room subscribers.
+	 * @param event Event raised on a closed connection.
+	 */
 	@EventListener
 	public void handleDisconnectEvent(SessionDisconnectEvent event) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
@@ -106,18 +139,44 @@ public class RoomConnectedEventListener {
 		}
 	}
 	
+	/**
+	 * Broadcasts a CONNECT message for a users nickname to all subscribers of the endpoint
+	 *  /topic/room/{roomNumber}/connections, excluding the connected user themselves.
+	 * @param user UUID of the user Principal.
+	 * @param roomNumber Valid room number the user connected to.
+	 */
 	private void handleUserConnect(String user, char roomNumber) {
 		broadcastConnect(user, roomNumber, ConnectionTypes.CONNECT);
 	}
 	
+	/**
+	 * Broadcasts a DISCONNECT message for a users nickname to all subscribers of the endpoint
+	 *  /topic/room/{roomNumber}/connections, excluding the disconnected user themselves.
+	 * @param user UUID of the user Principal.
+	 * @param roomNumber Valid room number the user disconnected from.
+	 */
 	private void handleUserDisconnect(String user, char roomNumber) {
 		broadcastConnect(user, roomNumber, ConnectionTypes.DISCONNECT);
 	}
 	
+	/**
+	 * Broadcasts a Rooms DTO to all subscribers of /topic/connections.
+	 * The DTO will consists of an array of Room DTOs, whereas each DTO holds a
+	 *  room number and count of all users currently subscribed to the endpoint
+	 *  /topic/room/{roomNumber}/messages.
+	 */
 	private void broadcastRoomsConnectionCount() {
 		template.convertAndSend(ENDPOINT_CONNECTIONS, roomTracker.createRoomsDto());
 	}
 	
+	/**
+	 * Broadcasts the CONNECT/DISCONNECT for a users nickname to all subscribers of that
+	 *  connected room number at endpoint /topic/room/{roomNumber}/connections,
+	 *  excluding the connecting/disconnecting user themselves.
+	 * @param user UUID of the user Principal.
+	 * @param roomNumber Valid room number the user connected to / disconnected from.
+	 * @param connectionType Value of CONNECT or DISCONNECT.
+	 */
 	private void broadcastConnect(String user, char roomNumber, ConnectionTypes connectionType) {
 		String endpointConnections = ENDPOINT_ROOM + roomNumber + ENDPOINT_SUFFIX_CONNECTIONS;
 		String nickname = usernameTracker.getNickname(user);
