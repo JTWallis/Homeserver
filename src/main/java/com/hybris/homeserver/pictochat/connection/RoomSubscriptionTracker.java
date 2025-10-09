6c3cad8acc4b3a0cdcc9ab82babf0966502afdd2
Justin <wallis.justin123@gmail.com>
2025-10-09 14:07:23 +0200

    feat: added methods to subscribe and unsubscribe a user to a room

--- src/main/java/com/hybris/homeserver/pictochat/connection/RoomSubscriptionTracker.java
+++ src/main/java/com/hybris/homeserver/pictochat/connection/RoomSubscriptionTracker.java
@@ -11,5 +11,15 @@ public class RoomSubscriptionTracker {
 
 	private final String[] ROOM_PREFIXES = {"a", "b", "c", "d"};
 	private Map<String, String> userToRoom = new ConcurrentHashMap<>();
+	
+	public void subscribe(String user, String room) {
+		System.out.println("User " + user + " connected to room " + room);
+		userToRoom.put(user, room);
+	}
+	
+	public void unsubscribe(String user) {
+		System.out.println("User " + user + " disconnected from a room.");
+		userToRoom.remove(user);
+	}
 
 }
