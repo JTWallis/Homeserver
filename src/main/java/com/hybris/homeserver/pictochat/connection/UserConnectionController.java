8c89d7444c911a53a0d48d61f284de4a7aa64568
Justin <wallis.justin123@gmail.com>
2025-10-09 20:38:14 +0200

    docs: added comment to registerUser method

--- src/main/java/com/hybris/homeserver/pictochat/connection/UserConnectionController.java
+++ src/main/java/com/hybris/homeserver/pictochat/connection/UserConnectionController.java
@@ -16,6 +16,17 @@ public class UserConnectionController {
 	@Autowired
 	private SimpMessagingTemplate template;
 
+	/**
+	 * Users can register themselves with their client-nickname, by publishing it to the endpoint /app/register.
+	 * The UUID of the user will be linked to that nickname, thus allowing for identical nicknames of different users.
+	 * On publishing to this endpoint, users should also subscribe to the endpoint /user/queue/receipts
+	 *  and await for the confirmation message to arrive to this endpoint.
+	 * This is done due to racing-conditions, as a UserConnection message may be broadcast, before the
+	 *  nickname was linked, even if the register-publish arrives before the room-number subscription.
+	 *  As a consequence, the clients would then receive a joining user message with the nickname NULL.
+	 * @param nickname Client nickname to link with the UUID.
+	 * @param principal Unique user id.
+	 */
 	@MessageMapping("/register")
 	public void registerUser(String nickname, Principal principal) {
 		System.out.println("Registering User " + nickname + " to UUID " + principal.getName());
