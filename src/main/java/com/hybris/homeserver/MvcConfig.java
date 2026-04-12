fca2e5e7b2c7ddbfdf5159d33cadde8394af8529
Justin Wallis <wallis.justin123@gmail.com>
2026-04-12 16:34:23 +0200

    feat: added mvc config with views for html templates

---
+++ src/main/java/com/hybris/homeserver/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.hybris.homeserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("cloud");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/cloud").setViewName("cloud");
+	}
+}
