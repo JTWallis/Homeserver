c55f8bfad081a566bc0e022925356ce71b140fef
Justin Wallis <wallis.justin123@gmail.com>
2026-04-12 16:49:40 +0200

    feat: implemented UserDetailsService specifically for cloud

---
+++ src/main/java/com/hybris/homeserver/cloud/CloudUserDetailsService.java
@@ -0,0 +1,49 @@
+package com.hybris.homeserver.cloud;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.hybris.homeserver.database.cloud.CloudLoginEntity;
+import com.hybris.homeserver.database.cloud.CloudLoginRepository;
+
+@Service
+public class CloudUserDetailsService implements UserDetailsService {
+	
+	private final CloudLoginRepository loginRepository;
+	
+	public CloudUserDetailsService(CloudLoginRepository loginRepository) {
+		this.loginRepository = loginRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		CloudLoginEntity entity;
+		
+		try {
+			entity = findByUsername(username);
+		} catch(EmptyResultDataAccessException e) {
+			throw new UsernameNotFoundException("No such user '" + username + "'");
+		}
+		
+		return User.builder()
+				.username(entity.getUsername())
+				.password(entity.getPassword())
+				.roles("USER")
+				.build();
+	}
+	
+	private CloudLoginEntity findByUsername(String username) throws EmptyResultDataAccessException {
+		CloudLoginEntity entity = loginRepository.getByUsername(username);
+		
+		if(entity == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
+		return entity;
+	}
+
+}
