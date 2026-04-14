164d9ad6eafec11b4d0d69074d9dc539f8d3665d
Justin Wallis <wallis.justin123@gmail.com>
2026-04-14 19:11:18 +0200

    feat: added service responsible for file upload and validating paths

---
+++ src/main/java/com/hybris/homeserver/endpoints/http/cloud/CloudStorageService.java
@@ -0,0 +1,59 @@
+package com.hybris.homeserver.endpoints.http.cloud;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class CloudStorageService {
+
+	public void store(String location, MultipartFile file) throws IOException, InvalidPathException, IllegalPathException {
+		if(location == null || location.isBlank() || file.isEmpty()) {
+			return;
+		}
+		
+		Path path = Paths.get(location);
+		
+		Path destination = path.resolve(
+				Paths.get(file.getOriginalFilename()))
+				.normalize()
+				.toAbsolutePath();
+		
+		// Security check destination path
+		if(!isPathLegal(destination) || !destination.getParent().equals(path.toAbsolutePath())) {
+			throw new IllegalPathException("Invalid path: " + destination.toString());
+		}
+		
+		try(InputStream istream = file.getInputStream()) {
+			Files.copy(istream, destination, StandardCopyOption.REPLACE_EXISTING);
+		}
+	}
+	
+	public Path getUserRoot() {
+		return Paths.get("/");
+	}
+	
+	public boolean isPathUserRoot(Path p) {
+		return getUserRoot().equals(p);
+	}
+	
+	public boolean isPathLegal(Path p)  {
+		// TODO: Check for subdirectory of user-root
+		return true;
+	}
+	
+	public boolean isPathLegal(String p) {
+		try {
+			return isPathLegal(Paths.get(p).normalize());
+		} catch(InvalidPathException e) {
+			return false;
+		}
+	}
+
+}
