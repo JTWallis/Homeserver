34f1d47458e60998f95b2bca7bc6f8921abf7634
Justin Wallis <wallis.justin123@gmail.com>
2026-04-23 23:44:40 +0200

    feat: created model for passing around novelai-api responses

---
+++ src/main/java/com/hybris/homeserver/endpoints/http/api/ai/NovelaiResponse.java
@@ -0,0 +1,22 @@
+package com.hybris.homeserver.endpoints.http.api.ai;
+
+public class NovelaiResponse {
+
+	private int statusCode;
+	private String message;
+	
+	public NovelaiResponse(int statusCode, String message) {
+		this.statusCode = statusCode;
+		this.message = message;
+	}
+	
+	public NovelaiResponse() {}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
