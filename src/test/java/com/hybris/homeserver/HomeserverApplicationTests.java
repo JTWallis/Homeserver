8ff9c94b36dd7fa2a8dc434f033f5d983307edc2
Justin Wallis <wallis.justin123@gmail.com>
2026-07-20 16:34:35 +0200

    fix: empty contextLoads test would throw exception

--- src/test/java/com/hybris/homeserver/HomeserverApplicationTests.java
+++ src/test/java/com/hybris/homeserver/HomeserverApplicationTests.java
@@ -2,8 +2,10 @@ package com.hybris.homeserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration	// Necessary to not throw exception in WebSocketConfig: https://stackoverflow.com/a/73614454
 class HomeserverApplicationTests {
 
 	@Test
