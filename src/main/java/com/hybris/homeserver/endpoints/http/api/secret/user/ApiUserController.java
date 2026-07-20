8b626ba7a82e53e13b5e125c4fc7d97a95197608
Justin Wallis <wallis.justin123@gmail.com>
2026-07-20 19:17:42 +0200

    feat: added GET for single and all users on sub-endpoint /existing

--- src/main/java/com/hybris/homeserver/endpoints/http/api/secret/user/ApiUserController.java
+++ src/main/java/com/hybris/homeserver/endpoints/http/api/secret/user/ApiUserController.java
@@ -1,11 +1,17 @@
 package com.hybris.homeserver.endpoints.http.api.secret.user;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hybris.homeserver.endpoints.http.api.ErrorResponseDto;
 import com.hybris.homeserver.endpoints.http.api.secret.auth.ApiUserUtils;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -32,4 +38,19 @@ public class ApiUserController {
 		return ResponseEntity.ok(new ApiUserResponseDto(dto));
 	}
 	
+	@GetMapping("/existing")
+	public ResponseEntity<?> get(@RequestParam("username") Optional<String> username, HttpServletRequest request) {
+		if(username.isEmpty() || username.get().isBlank()) {
+			return ResponseEntity.ok(userService.getAll());
+		}
+		
+		ApiUserResponseDto userDto = userService.getSingle(username.get());
+		if(userDto == null) {
+			return ResponseEntity.badRequest().body(new ErrorResponseDto(
+					HttpStatus.BAD_REQUEST, request.getRequestURI(), "No such user '" + username.get() + "'"
+			));
+		}
+		
+		return ResponseEntity.ok(userDto);
+	}
 }
